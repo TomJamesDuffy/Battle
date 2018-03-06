ea3aabed7272059a94355d26b86f0429626e2e4f
Raefe <raefe.nj@gmail.com>
2018-03-06 15:40:04 +0000

    some sessions stuff

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -4,18 +4,24 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   # ... app code here ...
 
-  get '/' do 
+  enable :sessions
+
+  get '/' do
     erb(:index)
   end
 
-  post '/names' do 
-    p params
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+  post '/names' do
+    @player1 = params[:Player_1_name]
+    @player2 = params[:Player_2_name]
+    session[:message] = "#{@player1} vs #{@player2}"
+    redirect('/play')
+  end
+
+  get '/play' do
+    @message = session[:message]
     erb(:play)
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -5,8 +5,8 @@ require 'capybara/rspec'
 feature 'User input' do
   scenario 'User can see input after submitting' do
     visit('/')
-      fill_in 'Player1', with: 'Raefe'
-      fill_in 'Player2', with: 'Tom'
+      fill_in 'Player_1_name', with: 'Raefe'
+      fill_in 'Player_2_name', with: 'Tom'
       click_button 'Submit'
     expect(page).to have_content 'Raefe vs Tom'
   end
