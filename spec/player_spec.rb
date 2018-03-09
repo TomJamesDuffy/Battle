fe770f50405a2b365e70e681910ab8f8d4feac40
Antonio <ciniglioantonio@gmail.com>
2018-03-09 16:02:56 +0000

    Implementing hit points - Write tests for Player#attack

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,31 @@ require 'player'
 
 describe Player do
   subject(:p) { Player.new('John') }
+  subject(:p2) { Player.new('Frank') }
 
   describe 'Player name' do
     it 'returns the player name' do
       expect(p.name).to eq 'John'
     end
   end
+
+  describe '#hitpoints' do
+    it 'returns the hit points' do
+      expect(p.hitpoints).to eq described_class::DEFAULT_HITPOINTS
+    end
+  end
+
+  describe '#attack' do
+   it 'damages the player' do
+     expect(p2).to receive(:reduce_hitpoints)
+     p.attack(p2)
+   end
+ end
+
+ describe '#reduce_hitpoints' do
+   it 'reduces the player hit points' do
+     expect { p.reduce_hitpoints }.to change { p.hitpoints }.by(-10)
+   end
+ end
+
 end
